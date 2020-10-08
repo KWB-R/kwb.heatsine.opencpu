96f4468981581082282e5bf6dd74eb4f29d55408
mrustl <michael.rustler@kompetenz-wasser.de>
2020-10-08 12:35:12 +0200

    Create package with R package kwb.pkgbuild
    for details see: R/.R_package_from_scratch

---
+++ R/.R_package_from_scratch.R
@@ -0,0 +1,34 @@
+### How to build an R package from scratch
+install.packages(pkgs = c("usethis", "remotes", "usethis", "fs"), repos = "https://cran.rstudio.com")
+remotes::install_github("kwb-r/kwb.pkgbuild")
+
+usethis::create_package(".")
+fs::file_delete(path = "DESCRIPTION")
+
+
+author <- list(name = "Michael Rustler",
+               orcid = "0000-0003-0647-7726",
+               url = "https://mrustl.de")
+
+pkg <- list(name = "kwb.heatsine.opencpu",
+            title = "Wrapper for R package kwb.heatsine for OpenCPU usage",
+            desc  = paste("Contains higher level functions build on top of R package kwb.heatsine",
+                          "for connecting with OPENCPU."))
+
+
+kwb.pkgbuild::use_pkg(author,
+                      pkg,
+                      version = "0.0.0.9000",
+                      stage = "experimental")
+
+
+usethis::use_vignette("Tutorial")
+
+### R functions
+
+
+kwb.pkgbuild::use_autopkgdown("kwb.heatsine.opencpu")
+
+kwb.pkgbuild::create_empty_branch_ghpages("kwb.heatsine.opencpu")
+
+kwb.pkgbuild::use_gitlab_ci_ghpages()
