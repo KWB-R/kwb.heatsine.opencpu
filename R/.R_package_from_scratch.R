95fa45a2d1f0940565aa4fe1b45618dc45aff915
mrustl <michael.rustler@kompetenz-wasser.de>
2020-10-08 13:36:43 +0200

    add Travis deploy keys for gh-pages

--- R/.R_package_from_scratch.R
+++ R/.R_package_from_scratch.R
@@ -1,7 +1,7 @@
 ### How to build an R package from scratch
 install.packages(pkgs = c("usethis", "remotes", "usethis", "fs"), repos = "https://cran.rstudio.com")
 remotes::install_github("kwb-r/kwb.pkgbuild")
-
+remotes::install_github("ropenscilabs/travis@v0.3.0.9004")
 usethis::create_package(".")
 fs::file_delete(path = "DESCRIPTION")
 
@@ -25,10 +25,12 @@ kwb.pkgbuild::use_pkg(author,
 usethis::use_vignette("Tutorial")
 
 ### R functions
-
+travis::
 
 kwb.pkgbuild::use_autopkgdown("kwb.heatsine.opencpu")
 
 kwb.pkgbuild::create_empty_branch_ghpages("kwb.heatsine.opencpu")
 
 kwb.pkgbuild::use_gitlab_ci_ghpages()
+
+travis::use_travis_deploy(key_name_private = "id_rsa", endpoint = ".com")
