cdc1f701d3fc7d61d5c6c199cd313a5c854f1f38
mrustl <michael.rustler@kompetenz-wasser.de>
2020-10-12 14:38:29 +0200

    update :book: (to fix Travis build)

--- R/run_optimisation.R
+++ R/run_optimisation.R
@@ -1,6 +1,20 @@
 #' JSON wrapper function for kwb.heatsine::run_optimisation
-#' @param json_args arguments for function \link[kwb.heatsine]{run_optimisation}
-#' in json format
+#' @param data_sw_selected data.frame with daily data temperature data of surface water monitoring point
+#' with columns "date" (format: "YYYY-MM-DD") and "value" (format: double, temperature in degree Celsius)
+#' for selected time period
+#' @param data_gw_selected data.frame with daily data temperature data of groundwater monitoring point
+#' with columns "date" (format: "YYYY-MM-DD") and "value" (format: double, temperature in degree Celsius)
+#' for selected time period
+#' @param retardation_factor hydraulic retardation factor (default: 2)
+#' @param sw_monitoring_id optional label for surface water monitoring id (default: "surface-water monitoring point"
+#' or attr(data_sw_selected, "monitoring_id") if data imported with  \code{\link{load_temperature_from_csv}}), otherwise
+#' can be any user-defined character string to be used as label for the monitoring point
+#' @param gw_monitoring_id optional label for groundwater monitoring id (default: "surface-water monitoring point"
+#' or attr(data_sw_selected, "monitoring_id") if data imported with  \code{\link{load_temperature_from_csv}}), otherwise
+#' can be any user-defined character string to be used as label for the monitoring point
+#' @param limits minimum/maximum period length for sinus optimisation in days (default: c(100, 500))
+#' @param tolerance the desired accuracy (default: 0.001
+#' @param debug show debug messages (default: FALSE)
 #' @return json list with sim/observation data ("data") fit parameters ("paras"), goodness-of-fit values ("gof")
 #' traveltimes ("traveltimes") and special (min, max, turning) points ("points") as returned by
 #' \link[kwb.heatsine]{get_predictions}
